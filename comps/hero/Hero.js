f98098398e4cafbd56d384539c90a3fdd5059ac3
Chris Ware <c.p.ware@hotmail.com>
2022-11-19 16:30:45 -0700

    Favicon and New Font

---
+++ comps/hero/Hero.js
@@ -0,0 +1,11 @@
+import Layout from '../layout/Layout'
+import styles from './Hero.module.css'
+
+const Hero = () => {
+  return (
+    <Layout>
+        <h1>Hero!!!</h1>
+    </Layout>
+  )
+}
+export default Hero
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Hero from "../comps/hero/Hero";
 import Nav from "../comps/Nav/Nav";
 
 export default function Home() {
   return (
     <>
       <Nav />
+      <Hero />
     </>
   );
 }
