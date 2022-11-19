c92334a50d505fc1ba1011c42cabd2d3163b2cc7
Chris Ware <c.p.ware@hotmail.com>
2022-11-19 00:10:50 -0700

    Nav

--- comps/Nav/Nav.js
+++ comps/Nav/Nav.js
@@ -3,9 +3,15 @@ import Link from "next/link";
 import Layout from "../layout/Layout";
 import styles from "./Nav.module.css";
 import Logo from "../logo/Logo";
-import Phone from '../../public/icons/phone.svg'
+import Phone from "../../public/icons/phone.svg";
+import Email from "../../public/icons/email.svg";
 
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const openMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <Layout>
       <header className={styles.container}>
@@ -14,7 +20,13 @@ const Nav = () => {
             <Logo />
           </div>
           <div className={styles.navMiddle}>
-            <ul className={styles.navMenu}>
+            <ul
+              className={
+                isOpen === false
+                  ? styles.navMenu
+                  : styles.navMenu + " " + styles.active
+              }
+            >
               <li>
                 <Link href='/' className={styles.navItem}>
                   Services
@@ -39,7 +51,20 @@ const Nav = () => {
           </div>
           <div className={styles.navRight}>
             <Phone className={styles.icon} />
+            <Email className={styles.icon} />
           </div>
+          <span
+            className={
+              isOpen === false
+                ? styles.hamburger
+                : styles.hamburger + " " + styles.active
+            }
+            onClick={openMenu}
+          >
+            <span className={styles.bar}></span>
+            <span className={styles.bar}></span>
+            <span className={styles.bar}></span>
+          </span>
         </nav>
       </header>
     </Layout>
