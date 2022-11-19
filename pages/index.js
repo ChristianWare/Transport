749d3ecd8375f10f683a6818a7ba2ba09effb1fc
Chris Ware <c.p.ware@hotmail.com>
2022-11-18 22:00:02 -0700

    Oswald Fonts

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div>
-      <h1>Yo man!!!</h1>
+      <h1>Providing A Professional and Reliable Service</h1>
     </div>
   );
 }
