8e542810c3b8fc315e62dc3c1314c30c1983d973
Chris Ware <c.p.ware@hotmail.com>
2022-11-19 17:55:18 -0700

    Hero 100VH Fix

---
+++ comps/services/Service.js
@@ -0,0 +1,11 @@
+import Layout from '../layout/Layout'
+import styles from './Services.module.css'
+
+const Service = () => {
+  return (
+    <Layout>
+        <h1>Services</h1>
+    </Layout>
+  )
+}
+export default Service
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Hero from "../comps/hero/Hero";
 import Nav from "../comps/Nav/Nav";
+import Service from "../comps/services/Service";
 
 export default function Home() {
   return (
     <>
       <Nav />
       <Hero />
+      <Service />
     </>
   );
 }
