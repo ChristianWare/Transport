ecb0d5adb67bec2a4668f645e940014cc0eb9db8
Chris Ware <c.p.ware@hotmail.com>
2022-11-19 17:42:03 -0700

    Hero Done

--- comps/hero/Hero.js
+++ comps/hero/Hero.js
@@ -1,11 +1,29 @@
-import Layout from '../layout/Layout'
-import styles from './Hero.module.css'
+import Layout from "../layout/Layout";
+import styles from "./Hero.module.css";
+import Image from "next/image";
+import Car from "../../public/images/sub.webp";
 
 const Hero = () => {
   return (
     <Layout>
-        <h1>Hero!!!</h1>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.left}>
+            <h1 className={styles.title}>
+              Providing A Professional & Reliable Service
+            </h1>
+            <p className={styles.copy}>
+              <span>//</span>We'll get you to where you need to go!
+            </p>
+          </div>
+          <div className={styles.right}>
+            <div className={styles.imgContainer}>
+              <Image src={Car} layout='responsive' objectFit='cover' />
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
-  )
-}
-export default Hero
+  );
+};
+export default Hero;
