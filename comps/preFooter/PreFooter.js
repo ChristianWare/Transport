ee27aafbfba2a827424af21c753194f6a113dca4
Chris Ware <c.p.ware@hotmail.com>
2022-11-20 17:34:43 -0700

    PreFooter Done

--- comps/preFooter/PreFooter.js
+++ comps/preFooter/PreFooter.js
@@ -1,11 +1,29 @@
-import Layout from '../layout/Layout'
-import styles from './PreFooter.module.css'
+import Layout from "../layout/Layout";
+import styles from "./PreFooter.module.css";
+import Image from "next/image";
+import Car from "../../public/images/sub2.png";
+import Button from "../button/Button";
 
 const PreFooter = () => {
   return (
-    <Layout>
-        <h1>Pre Footer Here!!!</h1>
-    </Layout>
-  )
-}
-export default PreFooter
+    <div className={styles.bgColor}>
+      <Layout>
+        <div className={styles.content}>
+          <div className={styles.left}>
+            <div className={styles.imgContainer}>
+              <Image src={Car} className={styles.img} />
+            </div>
+          </div>
+          <div className={styles.right}>
+            <h2 className={styles.title}>SCHEDULE YOUR NEXT RIDE WITH US!</h2>
+            
+            <div className={styles.btnContainer}>
+              <Button text='Book Now' btnType="white" />
+            </div>
+          </div>
+        </div>
+      </Layout>
+    </div>
+  );
+};
+export default PreFooter;
