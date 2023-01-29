f50d0b322a4913942d789d4a6d774b45d00bad0a
Chris Ware <c.p.ware@hotmail.com>
2023-01-28 18:55:36 -0700

    _Domcument Cleanup

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -14,14 +13,6 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <Script
-            strategy='afterInteractive'
-            dangerouslySetInnerHTML={{
-              __html: ``,
-            }}
-          />
-
-          {/* <div id='bro_reservation'></div><input type='hidden' id='cid' value='nVl0nR9tAGBmFccxf4xvDg%3d%3d' /><input type='hidden' id='dbaid' value='L4JAaOi0gExXw5xek9dmgQ%3d%3d' /> <script type='text/javascript'> (function () { var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true; po.src = 'https://bookridesonline.com/web/reservation.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s); })(); </script> */}
         </body>
       </Html>
     );
